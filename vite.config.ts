b76bb25b0cb041fac101934f3a8a30146a472e46
alshafii <alshafiimo@gmail.com>
2025-01-29 15:55:51 +0200

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: process.env.BASE_PATH || "/", // GitHub Pages needs this
+  base: "/links/", // GitHub Pages needs this
 });
